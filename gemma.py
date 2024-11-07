05743eb66841ba8c04a2cfcfd55f80338cb9bcd3
Juyeong Ji <bebechien@gmail.com>
2024-11-07 19:24:43 +0900

    Update gemma.py

--- gemma.py
+++ gemma.py
@@ -1,10 +1,16 @@
 """This module defines a game AI class implemented with Gemma"""
 
+import json
+import jsonschema
+import keras
 import keras_nlp
 import const
 
 from game import IGameAI
 
+
+# Run at half precision to save memory
+keras.config.set_floatx("bfloat16")
 
 class GameAI(IGameAI):
     """Class representing a game AI implemented with Gemma"""
@@ -12,6 +18,7 @@ class GameAI(IGameAI):
     __START_TURN_USER__ = "<start_of_turn>user\n"
     __START_TURN_MODEL__ = "<start_of_turn>model\n"
     __END_TURN__ = "<end_of_turn>\n"
+    __MAX_NUM_OF_TRY__ = 3
 
     model = None
 
@@ -19,13 +26,40 @@ class GameAI(IGameAI):
         super().__init__(app)
         self.model = keras_nlp.models.GemmaCausalLM.from_preset(
             self.__model_name__)
+        self.model.compile(sampler="top_k")
 
     def generate_event(self):
         """Generate and validate event."""
+        num_of_try = 0
+        prompt = f"{self.__START_TURN_USER__}{const.EVENT_GENERATION_PROMPT}{repr(const.EVENT_JSON_SCHEMA)}\nNo explanation required.{self.__END_TURN__}{self.__START_TURN_MODEL__}"
         while True:
-            event_string = self.model.generate_content(self.__START_TURN_USER__ + const.EVENT_GENERATION_PROMPT + repr(
-                const.EVENT_JSON_SCHEMA) + self.__END_TURN__ + self.__START_TURN_MODEL__)
-            print(event_string)
+            response = self.model.generate(prompt, max_length=1024)
+            print(response)
+            event_string = response.replace(prompt, "").removeprefix("```json").split("```")[0]  # Extract only the new response
+            try:
+                raw_event = json.loads(event_string)
+                event = {
+                    "text": f"{raw_event['properties']['text']}",
+                    "effect": raw_event['properties']['effect']
+                }
+                print(event)
+                jsonschema.validate(instance=event, schema=const.EVENT_JSON_SCHEMA)
+                return event
+
+            except Exception as e:
+                print(e)
+                print(event_string)
+                print("-"*80)
+                num_of_try += 1
+                if num_of_try > self.__MAX_NUM_OF_TRY__:
+                    print("Too many failure. Use example json instead.")
+                    return const.EVENT_JSON_EXAMPLE
+                print(f"Try again ({num_of_try}/{self.__MAX_NUM_OF_TRY__})")
 
     def random_event(self):
-        self.generate_event()
+        event = self.generate_event()
+        print(f"\nEvent: {event['text']}\n `-> Effect: {event['effect']}")
+        # Execute the effect of the event
+        effect = event['effect']
+        self.app.update_value(effect.get('supply', None), effect.get(
+            'health', None), effect.get('day', None))
