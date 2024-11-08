efeb02fc04d2f129f8e7298030579e4e01548b25
Ju-yeong Ji <bebechien@google.com>
2024-11-08 12:57:49 +0900

    fix gemma_ollama.py

--- gemma_ollama.py
+++ gemma_ollama.py
@@ -17,16 +17,20 @@ class GameAI(IGameAI):
     __END_TURN__ = "<end_of_turn>\n"
     __MAX_NUM_OF_TRY__ = 3
 
+    event_example = const.EVENT_JSON_EXAMPLE_STR
+
     def generate_event(self):
         """Generate and validate event."""
         num_of_try = 0
         prompt = const.EVENT_GENERATION_PROMPT.format(lang="")
         if self.app.lang == "ko":
             prompt = const.EVENT_GENERATION_PROMPT.format(lang=" in Korean")
+            self.event_example = const.EVENT_JSON_EXAMPLE_STR_KO
         elif self.app.lang == "ja":
             prompt = const.EVENT_GENERATION_PROMPT.format(lang=" in Japanese")
+            self.event_example = const.EVENT_JSON_EXAMPLE_STR_JA
 
-        chat_prompt = f"{self.__START_TURN_USER__}{prompt}{const.EVENT_JSON_EXAMPLE_STR}\nNo explanation required.{self.__END_TURN__}{self.__START_TURN_MODEL__}"
+        chat_prompt = f"{self.__START_TURN_USER__}{prompt}\nBelow is an example.\n{self.event_example}\nNo explanation required.{self.__END_TURN__}{self.__START_TURN_MODEL__}"
         while True:
             response = os.popen(
                 f"ollama run {self.__model_name__} \"{chat_prompt}\" > {self.__temp_filename__};cat {self.__temp_filename__}").read()
@@ -34,18 +38,27 @@ class GameAI(IGameAI):
                 "<end_of_turn>").split("```")[0]  # Extract only the new response
             try:
                 event = json.loads(event_string)
+
+                # fix common issues
+                if "supply" in event['effect']:
+                    event['effect']['supply'] = int(event['effect']['supply'])
+                if "health" in event['effect']:
+                    event['effect']['health'] = int(event['effect']['health'])
+                if "day" in event['effect']:
+                    event['effect']['day'] = int(event['effect']['day'])
+
                 jsonschema.validate(
                     instance=event, schema=const.EVENT_JSON_SCHEMA)
                 return event
 
-            except (jsonschema.exceptions.ValidationError, json.decoder.JSONDecodeError) as e:
+            except (KeyError, jsonschema.exceptions.ValidationError, json.decoder.JSONDecodeError) as e:
                 print(e)
                 print(event_string)
                 print("-"*80)
                 num_of_try += 1
                 if num_of_try > self.__MAX_NUM_OF_TRY__:
                     print("Too many failure. Use example json instead.")
-                    return json.loads(const.EVENT_JSON_EXAMPLE_STR)
+                    return json.loads(self.event_example)
                 print(f"Try again ({num_of_try}/{self.__MAX_NUM_OF_TRY__})")
 
     def random_event(self):
