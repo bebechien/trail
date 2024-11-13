e2df83380514e838f7406ed508b92029f5456f29
Juyeong Ji <bebechien@gmail.com>
2024-11-13 16:26:42 +0900

    implement flask app

--- game_flask.py
+++ game_flask.py
@@ -1,6 +1,6 @@
 """This module defines a game UI class implemented with Flask"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from inspect import currentframe
 import const
@@ -11,8 +11,10 @@ from game import IGameUI
 class GameUI(IGameUI):
     """Class representing a game UI implemented with Flask"""
     __GAME_TITLE__ = "The Kepler Trail"
+    __MAX_PARTY_NUMBER__ = 4
     app = None
     debug_info = ""
+    party_size = 1
 
     def __init__(self, lang="en", debug=False):
         super().__init__(lang, debug)
@@ -55,13 +57,32 @@ class GameUI(IGameUI):
                                    debug_info=self.debug_info
                                    )
 
-        @self.app.route('/get_party')
+        @self.app.route('/get_party', methods=['GET', 'POST'])
         def get_party():
+            if request.method == 'POST':
+                self.party_size = int(request.form['party_size'])
+
             return render_template('get_party.html',
                                    lang=self.lang,
                                    title=self.__GAME_TITLE__,
                                    debug_info=self.debug_info,
-                                   question=self.msg_json['input']['party_number']
+                                   question_num=self.msg_json['input']['party_number'],
+                                   question_name=self.msg_json['input']['member_name'],
+                                   max_party=self.__MAX_PARTY_NUMBER__,
+                                   party_size=self.party_size
                                    )
+
+        @self.app.route('/game_screen', methods=['POST'])
+        def game_screen():
+            if request.method == 'POST':
+                names = ""
+                for i in range(self.party_size):
+                    names += request.form[f"name_{i+1}"]
+                return render_template('game_screen.html',
+                                       lang=self.lang,
+                                       title=self.__GAME_TITLE__,
+                                       debug_info=self.debug_info,
+                                       names=names
+                                       )
 
         self.app.run(debug=self.debug)
